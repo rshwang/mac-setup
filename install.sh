3ca543819d6dbc4a616946b9b717aab12cf610b6
rshwang <rshwang@sina.cn>
2019-08-27 20:07:34 +0800

    更新软件安装方式并添加Mac配置命令

--- install.sh
+++ install.sh
@@ -2,36 +2,36 @@
 
 cat << -EOF
 #######################################################################
-# 当前脚本用于在运行OS X的电脑上安装应用程序
-# 原理为：利用homebrew作为OS X的包管理器
-#         brew install 安装命令行程序
-#         brew cask install 安装GUI程序
-#         Happy coding ~ Happy life.
+# 当前脚本用于在重装OS X系统的电脑上自动安装应用程序
+#       本程序利用homebrew作为OS X的包管理器
+#       brew install 安装命令行程序
+#       brew cask install 安装GUI程序
 #
-# Author: jsycdut <jsycdut@gmail.com>
-# Github: https://github.com/jsycdut/mac-setup
+# Author: R. Wang <rshwang@sina.cn>
+# Github: https://github.com/rshwang/mac-setup
 #
-# 祝使用愉快，有问题的话可以去GitHub提issue
+# Forked from: https://github.com/jsycdut/mac-setup
 #
-# 注意事项
+# 祝使用愉快。
 #
-# 1. OS X尽量保持较新版本，否则可能满足不了Homebrew的依赖要求
-# 2. 中途若遇见安装非常慢的情况，可用Ctrl+C打断，直接进行下一项的安装
 #######################################################################
 -EOF
 
 # 全局变量
 row_number=0
 column_number=0
+lo=0
+# 获取安装清单文件中的软件所在行列
 type=cli
+# 用于定位文件是用户界面还是命令行软件
 WD=`pwd`
 
 # 安装Homebrew并换TUNA源
 install_homebrew() {
-  if `command -v brew > /dev/null 2>&1`; then
+  if `command -v brew &> /dev/null`; then
     echo '👌  Homebrew已安装'
   else
-    echo '🍼  正在安装Homebrew'
+    echo '🍼  开始安装Homebrew'
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     if [[ $? -eq 0  ]]; then
       echo '🍻  Homebrew安装成功'
@@ -41,26 +41,24 @@ install_homebrew() {
     fi
   fi
 
-  echo '👍  为了让brew运行更加顺畅，将使用中国科学技术大学USTC提供的镜像，更新中，请等待...'
-  cd "$(brew --repo)"
-  git remote set-url origin https://mirrors.ustc.edu.cn/brew.git
+  echo '👍  为了让brew运行更加顺畅，将切换为清华大学TUNA提供的镜像，请稍后...'
+  git -C "$(brew --repo)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
 
-  cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
-  git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
+  git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
 
-  cd "$(brew --repo)"/Library/Taps/homebrew/homebrew-cask
-  git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-cask.git
+  git -C "$(brew --repo homebrew/cask)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git
 
   brew update
 }
 
-# 检查是否已安装某软件包
+# 检查软件包安装，传入软件包名称参数
 check_installation() {
   if [[ $type == "cli" ]]; then
     brew list -l | grep $1 > /dev/null
   else
     brew cask list -1 | grep $1 > /dev/null
   fi
+  # 通过list命令查看软件是否存在，如果存在返回值为0，否则返回1。
 
   if [[ $? -eq 0 ]]; then
      return 0
@@ -69,9 +67,10 @@ check_installation() {
   return 1
 }
 
-# 使用brew安装软件包
+# 使用brew命令安装软件包，传入软件包名称参数
 install() {
   check_installation $1
+  # 安装前先检查是否已安装
   if [[ $? -eq 0 ]]; then
     echo "👌 ==>已安装" $1 "，跳过..."
   else
@@ -80,60 +79,40 @@ install() {
       brew install $1 > /dev/null
       echo $?
     else
-      brew cask install $1 > /dev/null
+      brew cask install $1
     fi
 
     if [[ $? -eq 0 ]]; then
       echo "🍺 ==>安装成功 " $1
     else
-      echo "🚫 ==>安装失败 " $1
+      echo "🚫 ==>安装失败，请重试！ " $1
+      echo $1 >> "$type"".fail"
     fi
   fi
 }
 
-# 显示菜单
+# 根据用户输入内容显示安装软件列表，传入安装文件编号参数。
+# [0]:命令行软件列表文件；[1]:图形界面软件列表文件。
 show_menu() {
-  echo
-  read  -p "✨ 请选择要显示的软件包菜单列表类型 [0]命令行 [1]图形化(默认)：" ans
-  echo
-
-  case $ans in
-    0) cd $WD && cat cli.txt && type="cli"
+  case $1 in
+    0) echo "输出命令行软件列表："
+       cd $WD && cat cli.txt | awk '{print $1 ". " $2 "\t" $3}' && type="cli"   
     ;;
-    1) cd $WD && cat gui.txt && type="gui"
-    ;;
-    *) cd $WD && cat gui.txt && type="gui"
+    1) echo "输出图形化软件列表："
+       cd $WD && cat gui.txt | awk '{print $1 ". " $2 "\t" $3}' && type="gui"
     ;;
   esac
-
   echo
 }
 
-# 检查AWK是否可用
-check_awk() {
-  if ! `command -v awk > /dev/null`; then
-    echo 未检测到AWK，请先安装AWK再执行本程序...
-    exit 127
-  fi
-}
-
-# 利用awk，从cli.txt gui.txt两文件中截取软件包名称
+# 利用awk，从cli.txt gui.txt两文件中截取软件包名称。
+# 不要破坏cli.txt gui.txt文件排版。
+# 否则会导致软件包名称提取失败。
+# 分别传入软件包列表文件名称和所需软件所在行号。
 get_package_name() {
   local file_name=$1
   local row=$2
-  local col=$3
-  awk -v line=$row -v field=$col '{if(NR==line){print $field}}' $file_name
-}
-
-# 计算与软件名称编号对应的行和列号码
-# 不要破坏cli.txt gui.txt文件排版
-# 否则会导致计算行列值失败，进而无法提取出正确的软件包名称
-locate() {
-  local tmp=`expr $1 + 2`
-  row_number=`expr $tmp \/ 3`
-  tmp=`expr $1 % 3`
-  [ $tmp -eq 0 ] && tmp=3
-  column_number=`expr $tmp \* 3 - 1`
+  cat $file_name | grep -w $row | awk '{print $2}'
 }
 
 # 程序入口
@@ -141,18 +120,25 @@ echo
 echo "🙏  请花5秒时间看一下上述注意事项"
 sleep 5s
 install_homebrew
-while : ; do
-  show_menu
-  read  -p "✍️  请输入您想要安装的软件包的编号（多个软件包请用空格分隔，直接回车则全部安装）" ans
+while [[ $lo -lt 2 ]]; do
+  show_menu $lo
+  if [ -s "$type"".txt" ]; then
+    :
+  else
+    lo=`expr $lo + 1`
+    echo "该软件列表中没有内容，跳过安装！"
+    continue
+  fi
+    
+  read  -p "✍️ 请输入想要安装的软件包编号（多个请用空格分隔，回车安装全部：）" ans
   echo
   IFS=$'\n'
   read -d "" -ra arr <<< "${ans//' '/$'\n'}" # 本脚本中最喜欢的一句代码了
 
-  # 处理单纯的回车
+  # 如用户输入回车，将数组赋值为文件中的全部软件包编号
   if [[ "${#arr[@]}" -eq 0 ]]; then
     lines=`wc -l "$type"".txt" | awk '{printf $1}'`
-    count=`expr $lines \* 3`
-    for((i=0; i<$count; i++)); do
+    for((i=0; i<$lines; i++)); do
       arr[$i]=`expr $i + 1`
     done
   fi
@@ -164,39 +150,35 @@ while : ; do
       continue
     fi
 
-    locate $app
-    name=`get_package_name "$type"".txt" $row_number $column_number`
+    name=`get_package_name "$type"".txt" $app`
     [ -z "$name" ] && continue
     install $name
   done
 
-  read  -p "📕 是否继续查看菜单列表，Y/y继续，N/n退出 ：" ans
-  case $ans in
-    Y|y) :
-    ;;
-    *) break
-    ;;
-  esac
+  lo=`expr $lo + 1`
 done
+if [ -s gui.fail -o -s cli.fail ]; then
+  for stwr in `ls *.fail`; do
+    if [ -n $stwr ]; then
+      echo "❗️以下软件未成功安装，请检查并重新安装："
+      cat $stwr
+    fi
+  done
+fi
 
-# 更改回Homebrew的官方源
-cat << EOF
-  目前正在使用中国科学技术大学的Homebrew源，建议没有配置网络代理的同学，
-  保持现有状况，不要切回官方源，否则会导致以后安装软件包的下载速度缓慢，
-  对于配置了网络代理的同学，也可以选择将源切换回官方的源。
-EOF
+rm -rf *.fail
 
-sleep 1s
-read  -p "是否需要将Homebrew的源改回为官方源，[Y/y]确认，直接回车将跳过" ans
-case $ans in
-  Y|y)
-    echo "正在将Homebrew的源切换回官方源..."
-    cd "$(brew --repo)"/Library/Taps/homebrew/homebrew-cask
-    git remote set-url origin https://github.com/Homebrew/homebrew-cask
-    cd "$(brew --repo)"
-    git remote set-url origin https://github.com/Homebrew/brew.git
-    cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
-    git remote set-url origin https://github.com/Homebrew/homebrew-core
-    echo "已将Homebrew的源切换回官方源."
-  ;;
-esac
+echo "⁉️下面将进行电脑个性化设置，请稍后。⁉️"
+
+# 替换用户登录提示符...
+echo "正在调整命令提示符，请输入用户密码..."
+sudo sed -i "" -e '/^PS1/{s//# PS1/; a\
+PS1="\\u: \\W\\$ "
+}' /etc/bashrc && echo "命令提示符替换成功！" && source /etc/bashrc
+
+# 生成用户RSA秘钥，会提示输入密码，可直接回车跳过设置密码。
+
+echo "正在生成本地SSH Key..."
+ssh-keygen -t rsa -b 4096 -C "rshwang@sina.cn"
+
+echo "💗感谢您使用Mac自动配置程序，再见！💖"
